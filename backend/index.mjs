54d46d477259e40900f1b1ba1bf93f1925914a2f
Maitlla <maitlla@gmail.com>
2022-06-08 00:55:52 +0200

    First commit

---
+++ backend/index.mjs
@@ -0,0 +1,58 @@
+// en backend instalar: npm install express    |      npm install jsonwebtoken
+// para desinstalar un paquete equivocado: npm uninstall comosellame
+import express  from "express";
+import jwt from "jsonwebtoken";
+
+const app = express();
+
+const secret = "abc123.secreto.serio" // Esto debería de estar en un .env
+
+const user = {
+    username: "Daniel",
+    password: "abc123."
+}
+
+function decodeBasicToken(request) {
+    const [ authType, b64token ] = request.headers.authorization.split(" ")
+    const tokenBuffer = new Buffer.from(b64token, "base64")
+    const token = tokenBuffer.toString()
+    return token.split(":")
+}
+
+function authMiddleware (req, res, next) {
+    try {
+        const [ method, token ] = req.headers.authorization.split(" ")
+        const validAuth = jwt.verify(token,secret)
+        if ( validAuth ) {
+            const { username } = jwt.decode(token)
+            res.locals["username"] = username // Paso el user name para uso en los controllers
+            next()
+        } 
+    } catch (err) {
+        res.sendStatus(401)
+    }
+}
+
+app.get("/login/", (req, res)=>{
+    const [ username, password ] = decodeBasicToken(req)
+    if ( 
+        username === user.username && password === user.password
+    ) {
+        const token = jwt.sign(
+            {username},
+            secret,
+            {
+                expiresIn: "1h",
+            }
+        )
+        res.send(token)
+    } else {
+        res.sendStatus(401)
+    }
+})
+
+app.get("/secretos/", authMiddleware, (req, res)=>{
+    res.send(`Hola ${res.locals.username}. El secreto de la vida, el universo y de todo: 42`)
+})
+
+app.listen(3000, ()=>{console.log("Ready...");})
---
+++ frontend/src/services/ContextProvider.jsx
@@ -0,0 +1,61 @@
+import { createContext, useState } from 'react';
+
+import {backendURL, loginEndpoint, secretsEndpoint} from '../defines.mjs'
+
+export const context = createContext()
+
+function Context ({children}) {
+
+    const [ state, setState ] = useState({
+        token: null,
+        secrets: null,
+    })
+
+    const myContext = {
+        state,
+        actions: {
+            setToken: function (token) {
+                const newState = {...state, token}
+                setState(newState)
+            },
+            setSecrets: function (secrets) {
+                const newState = {...state, secrets}
+                setState(newState)
+            },
+            deleteToken: function () {
+                this.setToken(null)
+            },
+            getAPIToken: async function (username, password) {
+                const Authorization = `Basic ${btoa(username+':'+password)}`
+                const response = await fetch(backendURL+loginEndpoint,{
+                    headers: {
+                        Authorization
+                    }
+                })
+                if (response.status === 200) {
+                    this.setToken( await response.text() )
+                }
+            },
+            getAPISecrets: async function () {
+                const response = await fetch(backendURL+secretsEndpoint,{
+                    headers: {
+                        Authorization: "Bearer "+state.token
+                    }
+                })
+                if (response.status === 200 ) {
+                    this.setSecrets( await response.text() )
+                }
+            }
+        }
+    }
+    
+
+    return (
+        <context.Provider value={myContext}>
+            {children}
+        </context.Provider>
+    )
+
+}
+
+export default Context
